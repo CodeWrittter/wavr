212fc27667a0690c6b91d134fb3d05d87a9154da
Code Writter <jerry.sonhana@gmail.com>
2026-06-03 14:31:26 +0100

    Tries [1] : fixing empty black screen on launch (suspect JustAudioBackground.init)

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,38 @@ import 'services/download/download_queue.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  debugPrint("WAVR STEP 1");
+
   // init metadata_god (required before any tag read/write)
   // MetadataGod.initialize();
 
   // MUST be before runApp
-  await JustAudioBackground.init(
-    androidNotificationChannelId:   'com.wavr.app.channel.audio',
-    androidNotificationChannelName: 'Wavr Playback',
-    androidNotificationOngoing:     true,
-    androidStopForegroundOnPause:   true,
-  );
+  // await JustAudioBackground.init(
+  //   androidNotificationChannelId:   'com.wavr.app.channel.audio',
+  //   androidNotificationChannelName: 'Wavr Playback',
+  //   androidNotificationOngoing:     true,
+  //   androidStopForegroundOnPause:   true,
+  // );
+
+  debugPrint("WAVR STEP 2");
 
   final db    = AppDatabase();
+  debugPrint("WAVR STEP 3");
+
   final queue = DownloadQueue(db: db);
   final svc   = DownloadService(db: db, queue: queue);
+  debugPrint("WAVR STEP 4");
 
   await svc.initNotifications();
+  debugPrint("WAVR STEP 5");
   await svc.resumeQueue();
+  debugPrint("WAVR STEP 6");
 
   runApp(
     ProviderScope(
       child: WavrApp(db: db),
     ),
   );
+  debugPrint("WAVR STEP 7");
+
 }
